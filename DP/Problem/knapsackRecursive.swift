3bccb8f6acfa98b7e4e5e80be6ecd7f61e8365bc
Sunil-Kumar1 <Sunil.Kumar1@timesinternet.in>
2020-06-01 23:54:24 +0530

    memoize recursive function

--- DP/Problem/knapsackRecursive.swift
+++ DP/Problem/knapsackRecursive.swift
@@ -9,13 +9,20 @@
 import Foundation
 
 class KnapsackRecursive {
+    var t =  Array(repeating: Array(repeating:-1,count:8), count: 5)
     
     init() {
-       let p = getMaximumProfit(wt: [10,20,30], val: [60,100,120], W:50)
+        
+        //let p = minDistance("intention", "execution")
+        let p = getRecursiveMemoizeMaximumProfit(wt: [1,3,4,5], val: [1,4,5,7], W:7)
+        
+        
+        
+        //getRecursiveMaximumProfit(wt: [10,20,30], val: [60,100,120], W:50)
         
     }
     
-    func getMaximumProfit(wt: [Int], val: [Int], W: Int) -> Int {
+    func getRecursiveMaximumProfit(wt: [Int], val: [Int], W: Int) -> Int {
         var wa = wt
         var va = val
         let mw = W
@@ -29,11 +36,42 @@ class KnapsackRecursive {
         va.removeLast()
         
         if lastWt <= W {
-            return max(lastVal + getMaximumProfit(wt: wa, val: va, W: mw-lastWt) , getMaximumProfit(wt: wa, val: va, W: mw))
+            return max(lastVal + getRecursiveMaximumProfit(wt: wa, val: va, W: mw-lastWt) , getRecursiveMaximumProfit(wt: wa, val: va, W: mw))
         } else {
-            return getMaximumProfit(wt: wa, val: va, W: mw)
+            return getRecursiveMaximumProfit(wt: wa, val: va, W: mw)
         }
         
     }
-
+    
+    func getRecursiveMemoizeMaximumProfit(wt: [Int], val: [Int], W: Int) -> Int {
+        var wa = wt
+        var va = val
+        let mw = W
+        if wa.count == 0 || mw <= 0 {
+            return 0
+        }
+        
+        if t[wa.count][mw] != -1 {
+            return t[wa.count][mw]
+        }
+        
+        let lastWt = wa.last!
+        let lastVal = va.last!
+        wa.removeLast()
+        va.removeLast()
+        
+        if lastWt <= W {
+            
+            t[wa.count][mw] = max(lastVal + getRecursiveMemoizeMaximumProfit(wt: wa, val: va, W: mw-lastWt), getRecursiveMemoizeMaximumProfit(wt: wa, val: va, W: mw))
+            
+            return t[wa.count][mw]
+            
+        } else if lastWt > W {
+                t[wa.count][mw] = getRecursiveMemoizeMaximumProfit(wt: wa, val: va, W: mw)
+                return t[wa.count][mw]
+        }
+        
+        return t[wt.count][W]
+    }
+    
 }
